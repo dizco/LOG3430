093da525690837e879c62ecd7c560dae2666d2bf
dizco <gabriel.bourgault@kiosoft.ca>
2017-09-14 17:45:37 -0400

    Template de classe de tests

---
+++ TP1/src/main/java/edu/princeton/cs/algs4/GraphGeneratorTest.java
@@ -0,0 +1,86 @@
+package edu.princeton.cs.algs4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GraphGeneratorTest {
+
+	@Test
+	public void testSimpleIntInt() {
+		Graph g = GraphGenerator.simple(2, 1);
+		assertEquals("simple", g.getTypeName());
+		assertEquals(2, g.V());
+	}
+
+	@Test
+	public void testSimpleIntDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComplete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompleteBipartite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBipartiteIntIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBipartiteIntIntDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBinaryTree() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCycle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEulerianCycle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEulerianPath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWheel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRegular() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTree() {
+		fail("Not yet implemented");
+	}
+
+}
