0901795ac26d1157f366bb46ded387548543e11f
dizco <gabriel.bourgault@kiosoft.ca>
2017-09-26 20:29:05 -0400

    Ajout fichiers TP1

--- TP1/src/main/java/edu/princeton/cs/algs4/GraphGeneratorTest.java
+++ TP1/src/main/java/edu/princeton/cs/algs4/GraphGeneratorTest.java
@@ -6,81 +6,110 @@ import org.junit.Test;
 
 public class GraphGeneratorTest {
 
+	
+	
 	@Test
-	public void testSimpleIntInt() {
-		Graph g = GraphGenerator.simple(2, 1);
-		assertEquals("simple", g.getTypeName());
-		assertEquals(2, g.V());
+	public void testSimpleVE() {
+		Graph simple = GraphGenerator.simple(15,32);
+		assertEquals("Le graphe devrait être de type simple", simple.getTypeName() , "simple");
+		assertEquals("Le graphe devrait avoir 15 sommets", simple.V() , 15);
 	}
 
 	@Test
-	public void testSimpleIntDouble() {
-		fail("Not yet implemented");
+	public void testSimpleVP() {
+		Graph simple = GraphGenerator.simple(150,0.618281828);
+		assertEquals("Le graphe devrait être de type simple", simple.getTypeName() , "simple");
+		assertEquals("Le graphe devrait avoir 150 sommets", simple.V() , 150);
 	}
-
+	
 	@Test
 	public void testComplete() {
-		fail("Not yet implemented");
+		Graph complete = GraphGenerator.complete(1532);
+		assertEquals("Le graphe devrait être de type complete", complete.getTypeName() , "complete");
+		assertEquals("Le graphe devrait avoir 1532 sommets", complete.V() , 1532);
 	}
-
+	
 	@Test
 	public void testCompleteBipartite() {
-		fail("Not yet implemented");
+		Graph completeBipartite = GraphGenerator.completeBipartite(42,13);
+		assertEquals("Le graphe devrait être de type completeBipartite", completeBipartite.getTypeName() , "completeBipartite");
+		assertEquals("Le graphe devrait avoir 55 sommets", completeBipartite.V() , 42 + 13);
 	}
-
+	
 	@Test
-	public void testBipartiteIntIntInt() {
-		fail("Not yet implemented");
+	public void testBipartiteE() {
+		Graph bipartite = GraphGenerator.bipartite(44,33, 222);
+		assertEquals("Le graphe devrait être de type bipartite", bipartite.getTypeName() , "bipartite");
+		assertEquals("Le graphe devrait avoir 77 sommets", bipartite.V() , 77);
 	}
-
+	
 	@Test
-	public void testBipartiteIntIntDouble() {
-		fail("Not yet implemented");
+	public void testBipartiteP() {
+		Graph bipartite = GraphGenerator.bipartite(44,33, 0.75);
+		assertEquals("Le graphe devrait être de type bipartite", bipartite.getTypeName() , "bipartite");
+		assertEquals("Le graphe devrait avoir 77 sommets", bipartite.V() , 77);
 	}
-
+	
 	@Test
 	public void testPath() {
-		fail("Not yet implemented");
+		Graph path = GraphGenerator.path(123);
+		assertEquals("Le graphe devrait être de type path", path.getTypeName() , "path");
+		assertEquals("Le graphe devrait avoir 123 sommets", path.V() , 123);
 	}
-
+	
 	@Test
 	public void testBinaryTree() {
-		fail("Not yet implemented");
+		Graph binaryTree = GraphGenerator.binaryTree(321);
+		assertEquals("Le graphe devrait être de type binaryTree", binaryTree.getTypeName() , "binaryTree");
+		assertEquals("Le graphe devrait avoir 321 sommets", binaryTree.V() , 321);
 	}
-
+	
 	@Test
 	public void testCycle() {
-		fail("Not yet implemented");
+		Graph cycle = GraphGenerator.cycle(111);
+		assertEquals("Le graphe devrait être de type cycle", cycle.getTypeName() , "cycle");
+		assertEquals("Le graphe devrait avoir 111 sommets", cycle.V() , 111);
 	}
-
+	
 	@Test
 	public void testEulerianCycle() {
-		fail("Not yet implemented");
+		Graph eulerianCycle = GraphGenerator.eulerianCycle(12,21);
+		assertEquals("Le graphe devrait être de type eulerianCycle", eulerianCycle.getTypeName() , "eulerianCycle");
+		assertEquals("Le graphe devrait avoir 12 sommets", eulerianCycle.V() , 12);
 	}
-
+	
 	@Test
 	public void testEulerianPath() {
-		fail("Not yet implemented");
+		Graph eulerianPath = GraphGenerator.eulerianPath(23,32);
+		assertEquals("Le graphe devrait être de type eulerianPath", eulerianPath.getTypeName() , "eulerianPath");
+		assertEquals("Le graphe devrait avoir 23 sommets", eulerianPath.V() , 23);
 	}
-
+	
 	@Test
 	public void testWheel() {
-		fail("Not yet implemented");
+		Graph wheel = GraphGenerator.wheel(42);
+		assertEquals("Le graphe devrait être de type wheel", wheel.getTypeName() , "wheel");
+		assertEquals("Le graphe devrait avoir 42 sommets", wheel.V() , 42);
 	}
-
+	
 	@Test
 	public void testStar() {
-		fail("Not yet implemented");
+		Graph star = GraphGenerator.star(77);
+		assertEquals("Le graphe devrait être de type star", star.getTypeName() , "star");
+		assertEquals("Le graphe devrait avoir 77 sommets", star.V() , 77);
 	}
-
+	
 	@Test
 	public void testRegular() {
-		fail("Not yet implemented");
+		Graph regular = GraphGenerator.regular(66, 14);
+		assertEquals("Le graphe devrait être de type regular", regular.getTypeName() , "regular");
+		assertEquals("Le graphe devrait avoir 66 sommets", regular.V() , 66);
 	}
-
+	
 	@Test
 	public void testTree() {
-		fail("Not yet implemented");
+		Graph tree = GraphGenerator.tree(1000);
+		assertEquals("Le graphe devrait être de type tree", tree.getTypeName() , "tree");
+		assertEquals("Le graphe devrait avoir 1000 sommets", tree.V() , 1000);
 	}
-
 }
